af2df863b610366ee64c0c6ffe0126b7577358ac
周锐聪 <18823547629@163.com>
2019-11-17 16:09:59 +0800

    添加j彗星背景

--- components/background/Background.vue
+++ components/background/Background.vue
@@ -1,19 +1,23 @@
 <template>
     <!--制作一种日夜交替的背景-->
     <div id="background" class="background">
-        <Sun></Sun>
-        <Moon></Moon>
+        <div class="backgroundImg"></div>
+        <Sun v-if="false"></Sun>
+        <Moon v-if="false"></Moon>
+        <StarMgr></StarMgr>
     </div>
 </template>
 
 <script>
     import Moon from "./Moon";
     import Sun from "./Sun";
+    import StarMgr from "./StarMgr";
     export default {
         name: "Background.vue",
         components:{
             Moon,
-            Sun
+            Sun,
+            StarMgr
         }
     }
 </script>
@@ -22,6 +26,16 @@
     .background{
         width:100vw;
         height: 100vh;
-        background: #000;
+        overflow: hidden;
+        position: relative;
+        background: linear-gradient(rgba(48,46,48,1) 0,rgba(52,38,99,1) 100%);
+    }
+    .backgroundImg{
+        position: fixed;
+        top:0;
+        left:0;
+        width:100%;
+        height: 100%;
+        -webkit-backdrop-filter: blur(5px);
     }
 </style>
---
+++ components/background/StarMgr.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="star_mgr">
+        <div class="wrap">
+            <Star v-for="(item,index) of starList" :key="'star'+index" :index="index"></Star>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Star from "./Star";
+    export default {
+        name: "StarMgr.vue",
+        data(){
+          return{
+              starList:new Array(14)
+          }
+        },
+        components:{
+            Star
+        }
+    }
+</script>
+
+<style scoped>
+    .star_mgr{
+        position: fixed;
+        width:100%;
+        height: 100%;
+        top:0%;
+        left:0;
+        transform: translateY(50%);
+    }
+    .wrap{
+        width:100%;
+        height: 100%;
+        position: relative;
+    }
+</style>
